90d0072266683b158c66f8b1faa34fecf5031c35
Sergio Lazaris <sergio_lazaris@estudante.sesisenai.org.br>
2025-10-08 17:27:35 -0300

    Agr deixa sapoha bonita e upa pro render ;)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask(__name__, static_folder='../static', template_folder='templates')
+app = Flask(__name__)
 
-from app import view 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY'] = '276152493yv7n8h2c8fr8234ohsdrn1r8ogrdb8'
+
+
+db=SQLAlchemy(app)
+migrate=Migrate(app, db)
+
+from app import view
+from app.model import Questao
--- app/view.py
+++ app/view.py
@@ -1,6 +1,45 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, request, redirect, url_for
+from app.model import Questao, Opcao, Resposta
+from app.form import RespostaForm
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/questionario', methods=['POST', 'GET'])
+def questionario():
+    questoes = Questao.query.order_by('id')
+    opcoes = Opcao.query.order_by('id')
+    form = RespostaForm()
+
+    if request.method == 'POST':
+        for questao in questoes:
+            opcao_id = request.form.get(f'questao_{questao.id}')
+            if opcao_id:
+                resposta = Resposta(questao_fk=questao.id, opcao_fk=int(opcao_id))
+                db.session.add(resposta)
+        db.session.commit()
+        return redirect(url_for('homepage'))  # Redireciona após o envio
+
+    return render_template('questionario.html', questoes=questoes, opcoes=opcoes, form=form)
+
+
+@app.route('/resultados')
+def resultados():
+    questoes = Questao.query.order_by('id').all()
+    dados = []
+    for questao in questoes:
+        opcoes = []
+        votos = []
+        for opcao in questao.opcoes:
+            opcoes.append(opcao.text)
+            votos.append(Resposta.query.filter_by(opcao_fk=opcao.id).count())
+        dados.append({
+            'questao': questao.text,
+            'opcoes': opcoes,
+            'votos': votos
+        })
+    return render_template('resultados.html', dados=dados)
+
