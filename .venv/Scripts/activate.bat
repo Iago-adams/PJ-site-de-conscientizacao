03303a4b5cc3c0150ff9b85bd9b8eea3140f319c
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-10-14 07:57:45 -0300

    arrumando venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sszfa\Documents\Coding\PJ-site-de-conscientizacao\.venv
+set VIRTUAL_ENV=C:\Users\bianca_vaz\Documents\GitHub\PJ-site-de-conscientizacao\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
