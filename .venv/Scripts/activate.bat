dc15a5ac3603ef8f91ebe4f2347ac39d154d2359
iago-adams <adams.iiago@gmail.com>
2025-11-24 21:53:54 -0300

    requirements com o gunicorn

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\iago_adams\Documents\a\PJ-site-de-conscientizacao\.venv"
+set "VIRTUAL_ENV=C:\Users\Desktop\Desktop\python\PJ-site-de-conscientizacao\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
